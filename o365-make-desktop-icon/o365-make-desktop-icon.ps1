3aba53bea185ce20a2c3de9e5c8272b32ca1494b
Jeff Jones <spjeff@spjeff.com>
2016-05-26 12:40:18 -0500

    Code
    * make icon with parameters

--- o365-make-desktop-icon/o365-make-desktop-icon.ps1
+++ o365-make-desktop-icon/o365-make-desktop-icon.ps1
@@ -9,13 +9,14 @@ $pw = Read-Host "Tenant - Password" -AsSecureString
 $hash = $pw | ConvertFrom-SecureString
 
 # shortcut
-"`$h = $hash`n`$secpw = ConvertTo-SecureString -String `$h`n`$c = New-Object System.Management.Automation.PSCredential (""$user"", `$secpw)`nConnect-SPOService -URL $url -Credential `$c`nImport-Module -WarningAction SilentlyContinue Microsoft.Online.SharePoint.PowerShell" | Out-File "$home\o365-icon.ps1"
 
 # create shortcut
 $folder = [Environment]::GetFolderPath("Desktop")
-$TargetFile = "C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe -NoExit ""$home\o365-icon.ps1"""
+$TargetFile = "C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe"
 $ShortcutFile = "$folder\Office365.lnk"
 $WScriptShell = New-Object -ComObject WScript.Shell
 $Shortcut = $WScriptShell.CreateShortcut($ShortcutFile)
+$Shortcut.Arguments = " -NoExit ""$home\o365-icon.ps1"""
+$Shortcut.IconLocation = "powershell.exe, 0";
 $Shortcut.TargetPath = $TargetFile
 $Shortcut.Save()
