550cecea1fee4e935d57f120d854a3bdffd01fd6
Jeff Jones <spjeff@spjeff.com>
2017-01-29 10:52:11 -0600

    Code
    VSCode format doc

--- Replace-SEWP-Content.ps1
+++ Replace-SEWP-Content.ps1
@@ -16,143 +16,144 @@
 #>
 
 param(
-	[string]$url,					# Target Site Collection URL.  Loops all child webs.
-	[string]$oldText,				# Old string to find.
-	[string]$newText,				# New string to replace with.
-	[switch]$readOnly,				# Read only operation.  Checks for Web Parts but will not change any content.
-	[string]$overwriteWebPartTitle	# Name of Web Part Title to locate and update HTML source content.
+    [string]$url,					# Target Site Collection URL.  Loops all child webs.
+    [string]$oldText,				# Old string to find.
+    [string]$newText,				# New string to replace with.
+    [switch]$readOnly,				# Read only operation.  Checks for Web Parts but will not change any content.
+    [string]$overwriteWebPartTitle	# Name of Web Part Title to locate and update HTML source content.
 )
 
 # Plugins
 Add-PSSnapIn Microsoft.SharePoint.PowerShell -ErrorAction SilentlyContinue | Out-Null
 
 function processPage ($web, $serverRelativeUrl) {
-	# Inspect Web Parts on a given page
-	$manager = $web.GetLimitedWebPartManager($serverRelativeUrl, [System.Web.UI.WebControls.WebParts.PersonalizationScope]::Shared);
+    # Inspect Web Parts on a given page
+    $manager = $web.GetLimitedWebPartManager($serverRelativeUrl, [System.Web.UI.WebControls.WebParts.PersonalizationScope]::Shared);
 	
-	# Exclude already processed ASPX pages
-	$found = $global:webPartsToUpdate |? {$_.FileURL -eq $manager.ServerRelativeUrl}
-	if (!$found) {
-		Write-Host "FILE: " $manager.ServerRelativeUrl
-		$webParts = $manager.WebParts;
+    # Exclude already processed ASPX pages
+    $found = $global:webPartsToUpdate |? {$_.FileURL -eq $manager.ServerRelativeUrl}
+    if (!$found) {
+        Write-Host "FILE: " $manager.ServerRelativeUrl
+        $webParts = $manager.WebParts;
 
-		foreach ($webPart in $webParts) {
-			# Update Content Editor and Script Editor Web Parts
-			$type = $null
-			if ($webPart.GetType().ToString()-eq "Microsoft.SharePoint.WebPartPages.ContentEditorWebPart") {
-				$content = $webPart.Content.InnerText
-				if ($content -like "*$oldText*") {
-					$type = "CEWP"
-				}
-			}
-			if ($webPart.GetType().ToString() -eq "Microsoft.SharePoint.WebPartPages.ScriptEditorWebPart") {
-				$content = $webPart.Content
-				if ($content -like "*$oldText*") {
-					$type = "SEWP"
-				}
-			}
-			if ($type) {
-				if ($overwriteWebPartTitle) {
-					if ($overwriteWebPartTitle -ne $webPart.Title) {
-						break
-					}
-				}
-				# Used as a check before committing updates
-				$o = New-Object PSObject;
-				$o | Add-Member -MemberType Noteproperty -Name Title -Value $manger.Url
-				$o | Add-Member -MemberType Noteproperty -Name Type -Value $webPart.GetType()
-				$o | Add-Member -MemberType Noteproperty -Name Content -Value $content
-				$o | Add-Member -MemberType Noteproperty -Name FileURL -Value $manager.ServerRelativeUrl
-				$global:webPartsToUpdate += $o;
+        foreach ($webPart in $webParts) {
+            # Update Content Editor and Script Editor Web Parts
+            $type = $null
+            if ($webPart.GetType().ToString()-eq "Microsoft.SharePoint.WebPartPages.ContentEditorWebPart") {
+                $content = $webPart.Content.InnerText
+                if ($content -like "*$oldText*") {
+                    $type = "CEWP"
+                }
+            }
+            if ($webPart.GetType().ToString() -eq "Microsoft.SharePoint.WebPartPages.ScriptEditorWebPart") {
+                $content = $webPart.Content
+                if ($content -like "*$oldText*") {
+                    $type = "SEWP"
+                }
+            }
+            if ($type) {
+                if ($overwriteWebPartTitle) {
+                    if ($overwriteWebPartTitle -ne $webPart.Title) {
+                        break
+                    }
+                }
+                # Used as a check before committing updates
+                $o = New-Object PSObject;
+                $o | Add-Member -MemberType Noteproperty -Name Title -Value $manger.Url
+                $o | Add-Member -MemberType Noteproperty -Name Type -Value $webPart.GetType()
+                $o | Add-Member -MemberType Noteproperty -Name Content -Value $content
+                $o | Add-Member -MemberType Noteproperty -Name FileURL -Value $manager.ServerRelativeUrl
+                $global:webPartsToUpdate += $o;
 					
-				if (!$readOnly) {
-					Write-Host ("Updating: " + $webPart.Title)
-					if ($type -eq "CEWP") {
-						# Content Editor
-						if ($overwriteWebPartTitle) {
-							# Overwrite
-							$xmlDoc = New-Object xml
-							$newXmlElement = $xmlDoc.CreateElement("NewContent")
-							$newXmlElement.InnerText = $newText
+                if (!$readOnly) {
+                    Write-Host ("Updating: " + $webPart.Title)
+                    if ($type -eq "CEWP") {
+                        # Content Editor
+                        if ($overwriteWebPartTitle) {
+                            # Overwrite
+                            $xmlDoc = New-Object xml
+                            $newXmlElement = $xmlDoc.CreateElement("NewContent")
+                            $newXmlElement.InnerText = $newText
 			 
-							# Save
-							$webPart.Content = $newXmlElement
-							$manager.SaveChanges($webPart)
-						} else {
-							# Load Old Content
-							$oldXmlElement = $webPart.Content
-							$oldXmlContent = $oldXmlElement.InnerText
+                            # Save
+                            $webPart.Content = $newXmlElement
+                            $manager.SaveChanges($webPart)
+                        } else {
+                            # Load Old Content
+                            $oldXmlElement = $webPart.Content
+                            $oldXmlContent = $oldXmlElement.InnerText
 							
-							# Replace
-							$xmlDoc = New-Object xml
-							$newXmlElement = $xmlDoc.CreateElement("NewContent")
-							$newXmlElement.InnerText = $oldXmlContent.Replace($oldText, $newText)
+                            # Replace
+                            $xmlDoc = New-Object xml
+                            $newXmlElement = $xmlDoc.CreateElement("NewContent")
+                            $newXmlElement.InnerText = $oldXmlContent.Replace($oldText, $newText)
 			 
-							# Save
-							$webPart.Content = $newXmlElement
-							$manager.SaveChanges($webPart)
-						}
-					}
-					if ($type -eq "SEWP") {
-						# Script Editor
-						if ($overwriteWebPartTitle) {
-							# Overwrite
-							$webPart.Content = $newText
-							$manager.SaveChanges($webPart)
-						} else {
-							# Save
-							$old = $webPart.Content
-							$new = $old.Replace($oldText, $newText)
-							$webPart.Content = $new
-							$manager.SaveChanges($webPart)
-						}
-					}
-				}
-			}
-		}
-	}
+                            # Save
+                            $webPart.Content = $newXmlElement
+                            $manager.SaveChanges($webPart)
+                        }
+                    }
+                    if ($type -eq "SEWP") {
+                        # Script Editor
+                        if ($overwriteWebPartTitle) {
+                            # Overwrite
+                            $webPart.Content = $newText
+                            $manager.SaveChanges($webPart)
+                        } else {
+                            # Save
+                            $old = $webPart.Content
+                            $new = $old.Replace($oldText, $newText)
+                            $webPart.Content = $new
+                            $manager.SaveChanges($webPart)
+                        }
+                    }
+                }
+            }
+        }
+    }
 }
 
 function processLibrary ($web, $documentLibraryTitle) {
-	# Process all ASPX within a given Document Library
-	try {
-		$list = $web.Lists[$documentLibraryTitle]
-		if ($list) {
-			foreach ($item in $list.Items) {
-				processPage $web $item.File.ServerRelativeUrl
-			}
-		}
-	} catch {}
+    # Process all ASPX within a given Document Library
+    try {
+        $list = $web.Lists[$documentLibraryTitle]
+        if ($list) {
+            foreach ($item in $list.Items) {
+                processPage $web $item.File.ServerRelativeUrl
+            }
+        }
+    } catch {
+    }
 }
 
 function processWeb ($web) {
-	# Process only ASPX web part pages
-	Write-Host "WEB:  " $web.Url
+    # Process only ASPX web part pages
+    Write-Host "WEB:  " $web.Url
 	
-	# Homepage
-	processPage $web $web.RootFolder.WelcomePage
+    # Homepage
+    processPage $web $web.RootFolder.WelcomePage
 	
-	# /SitePages/ and /Pages/ library
-	processLibrary $web "Site Pages"
-	processLibrary $web "Pages"
+    # /SitePages/ and /Pages/ library
+    processLibrary $web "Site Pages"
+    processLibrary $web "Pages"
 }
 
 $global:webPartsToUpdate = @()
 function Main() {
-	# Display mode
-	if ($readOnly) {
-		Write-Host "[READ ONLY MODE]" -ForegroundColor Yellow
-	} else {
-		Write-Host "[UPDATE MODE]" -ForegroundColor Green
-	}
+    # Display mode
+    if ($readOnly) {
+        Write-Host "[READ ONLY MODE]" -ForegroundColor Yellow
+    } else {
+        Write-Host "[UPDATE MODE]" -ForegroundColor Green
+    }
 	
-	# Process all webs in target Site Collection
-	$site = Get-SPSite $url
-	$site.AllWebs |% {processWeb $_}
-	$site.Dispose()
+    # Process all webs in target Site Collection
+    $site = Get-SPSite $url
+    $site.AllWebs |% {processWeb $_}
+    $site.Dispose()
 	
-	# Display
+    # Display
     Write-Host "`nDONE" -ForegroundColor Green
-	$global:webPartsToUpdate | Format-List
+    $global:webPartsToUpdate | Format-List
 }
 Main
