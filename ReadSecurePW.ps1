1ba78d86d517c576671afaf42b76dd2a81c1999e
Jeff Jones <spjeff@spjeff.com>
2017-01-29 10:52:03 -0600

    Code
    VSCode format doc

--- ReadSecurePW.ps1
+++ ReadSecurePW.ps1
@@ -16,45 +16,47 @@
 #>
 
 param (
-	[Alias("c")]
-	[switch]$clearSavedPW	
+    [Alias("c")]
+    [switch]$clearSavedPW	
 )
 
 Function GetSecurePassword($user) {
-	# Registry HKCU folder
-	$path = "HKCU:\Software\AdminScript"
-	if (!(Test-Path $path)) {md $path | Out-Null}
-	$name = $user
+    # Registry HKCU folder
+    $path = "HKCU:\Software\AdminScript"
+    if (!(Test-Path $path)) {
+        md $path | Out-Null
+    }
+    $name = $user
 	
-	# Do we need to clear old paswords?
-	if ($clearSavedPW) {
-		Remove-ItemProperty -Path $path -Name $name -Confirm:$false -ErrorAction SilentlyContinue
-		Write-Host "Deleted password OK for $name" -Fore Yellow
-		Exit
-	}
+    # Do we need to clear old paswords?
+    if ($clearSavedPW) {
+        Remove-ItemProperty -Path $path -Name $name -Confirm:$false -ErrorAction SilentlyContinue
+        Write-Host "Deleted password OK for $name" -Fore Yellow
+        Exit
+    }
 	
-	# Do we have registry HKCU saved password?
-	$hash = (Get-ItemProperty -Path $path -Name $name -ErrorAction SilentlyContinue)."$name"
+    # Do we have registry HKCU saved password?
+    $hash = (Get-ItemProperty -Path $path -Name $name -ErrorAction SilentlyContinue)."$name"
 	
-	# Prompt for input
-	if (!$hash) {
-		$sec = Read-Host "Enter Password for $name" -AsSecureString
-		if (!$sec) {
-			Write-Error "Exit - No password given"
-			Exit
-		}
-		$hash = $sec | ConvertFrom-SecureString
+    # Prompt for input
+    if (!$hash) {
+        $sec = Read-Host "Enter Password for $name" -AsSecureString
+        if (!$sec) {
+            Write-Error "Exit - No password given"
+            Exit
+        }
+        $hash = $sec | ConvertFrom-SecureString
 		
-		# Prompt to save to HKCU
-		$save = Read-Host "Save to HKCU registry (secure hash) [Y/N]?"
-		if ($save -like "Y*") {
-			Set-ItemProperty -Path $path -Name $name -Value $hash -Force
-			Write-Host "Saved password OK for $name" -Fore Yellow
-		}
-	}
+        # Prompt to save to HKCU
+        $save = Read-Host "Save to HKCU registry (secure hash) [Y/N]?"
+        if ($save -like "Y*") {
+            Set-ItemProperty -Path $path -Name $name -Value $hash -Force
+            Write-Host "Saved password OK for $name" -Fore Yellow
+        }
+    }
 	
-	# Return
-	return $hash
+    # Return
+    return $hash
 }
 
 # Example usage for SharePoint Online (Office 365)
