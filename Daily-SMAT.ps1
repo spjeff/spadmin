2a3e9de647b9029eccc57935e67400bd8c928bcc
Jeff Jones <spjeff@spjeff.com>
2017-07-12 11:40:46 -0500

    Code
    * detect path
    * parameters
    * output folder \REPORT\

--- Daily-SMAT.ps1
+++ Daily-SMAT.ps1
@@ -5,8 +5,8 @@
 .NOTES
 	File Name		: Daily-SMAT.ps1
 	Author			: Jeff Jones - @spjeff
-	Version			: 0.10
-	Last Modified	: 05-22-2017
+	Version			: 0.11
+	Last Modified	: 06-29-2017
 	
 .PARAMETER install
 	Typing "Daily-SMAT.ps1 -install" will create a local Task Scheduler job under credentials of the current user. Job runs once daily at 3AM to produce a new current report.
@@ -25,12 +25,12 @@ param (
 )
 
 Function Installer() {
-	# Add to Task Scheduler
-	Write-Output "  Installing to Task Scheduler..."
-	if(!$user) {
-		$user = $ENV:USERDOMAIN + "\"+$ENV:USERNAME
-	}
-	Write-Output "  User for Task Scheduler job: $user"
+    # Add to Task Scheduler
+    Write-Output "  Installing to Task Scheduler..."
+    if (!$user) {
+        $user = $ENV:USERDOMAIN + "\" + $ENV:USERNAME
+    }
+    Write-Output "  User for Task Scheduler job: $user"
 	
     # Attempt to detect password from IIS Pool (if current user is local admin and farm account)
     $appPools = Get-WMIObject -Namespace "root/MicrosoftIISv2" -Class "IIsApplicationPoolSetting" | Select-Object WAMUserName, WAMUserPass
@@ -48,30 +48,43 @@ Function Installer() {
         $pass = Read-Host "Enter password for $user "
     }
 	
-	# Task Scheduler command
-	$cmd = "-ExecutionPolicy Bypass ""$cmdpath"""
+    # Task Scheduler command
+    $cmd = "powershell.exe -ExecutionPolicy Bypass ""$cmdpath"""
 	
-	# Delete task
-	Write-Output "SCHTASKS DELETE"
-	SCHTASKS /delete /tn "Daily-SMAT" /f
-	Write-Host "  [OK]" -Fore Green
+    # Delete task
+    Write-Output "SCHTASKS DELETE"
+    SCHTASKS /delete /tn "Daily-SMAT" /f
+    Write-Host "  [OK]" -Fore Green
 	
-	# Create task
-	Write-Output "SCHTASKS CREATE"
-	SCHTASKS /create /tn "Daily-SMAT" /ru $user /rp $pass /rl highest /st 03:00
-	Write-Host "  [OK]" -Fore Green
+    # Create task
+    Write-Output "SCHTASKS CREATE"
+    SCHTASKS /create /tn "Daily-SMAT" /tr "$cmd" /ru $user /rp $pass /rl highest /sc Daily /st 21:00
+    Write-Host "  [OK]" -Fore Green
 }
 
 # Main
+$cmdpath = $MyInvocation.MyCommand.Path
+$cmdfolder = Split-Path $cmdpath
 if ($install) {
-	Installer
-} else {
-	Start-Transcript
-	$cmdpath = $MyInvocation.MyCommand.Path
-	$cmdfolder = Split-Path $cmdpath
-	$date = (Get-Date).ToString("yyyy-MM-dd")
-	"  Output: $cmdfolder\$date\"
-	.\SMAT.exe -o "$cmdfolder\$date\" -t 2 -q
-	Write-Host "DONE" -Fore Green
-	Stop-Transcript
+    Installer
+}
+else {
+    Start-Transcript
+    # Remove folders older than 30 days
+    md "$cmdfolder\REPORT" -ErrorAction SilentlyContinue | Out-Null
+    $threshold = (Get-Date).AddDays(-30)
+    $folders = Get-ChildItem "$cmdfolder\REPORT"
+    foreach ($f in $folders) {
+        if ($f.LastWriteTime -lt $threshold) {
+            Write-Host "Deleting folder $($f.Name)"
+            $f | Remove-ChildItem -Confirm:$false
+        }
+    }
+
+    # Execute SMAT report and output to folder with today's date
+    $date = (Get-Date).AddDays(1).ToString("yyyy-MM-dd")
+    "  Output: $cmdfolder\$date\"
+    Invoke-Expression "$cmdfolder\SMAT\SMAT.exe -o ""$cmdfolder\REPORT\$date\"" -t 8 -q"
+    Write-Host "DONE" -Fore Green
+    Stop-Transcript
 }
