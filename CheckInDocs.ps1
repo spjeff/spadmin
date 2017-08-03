b8e65f8fb053bf92bddedd84cd7f0af33c843c10
Jeff Jones <spjeff@spjeff.com>
2017-08-03 12:49:48 -0500

    Code
    * check-in documents with no checked out version

---
+++ CheckInDocs.ps1
@@ -0,0 +1,30 @@
+﻿[CmdletBinding()]
+param (
+	[Parameter(Mandatory=$true, ValueFromPipeline=$false, HelpMessage='URL of site collection')]
+	[string]$url
+)
+
+# Check in documents with no published version.
+function CheckInDocs ($url) {
+    $site = Get-SPSite $url
+	# Loop all webs
+    foreach($web in $site.AllWebs) {
+		# Only Document Libraries
+        foreach($list in $web.GetListsOfType([Microsoft.SharePoint.SPBaseType]::DocumentLibrary)) {
+			# Take over checkout
+            $list.CheckedOutFiles | % { $_.TakeOverCheckOut() }
+			# Force check in
+            $list.CheckedOutFiles | % {
+                $item = $list.GetItemById($_.ListItemId)
+                $item.File.CheckIn("File checked in by administrator")
+                Write-Host $item.File.ServerRelativeUrl -NoNewline; Write-Host " Checked in " -ForegroundColor Green
+            }
+        }
+		# Clean memory
+        $web.dispose();
+    }
+    $site.dispose();
+}
+
+# Invoke
+CheckInDocs $url
