1042a45cb346477f7f68d0c7998058740487f887
Jeff Jones <spjeff@spjeff.com>
2020-07-13 11:36:38 -0500

    Mouse Move PS1

--- mm.ps1
+++ mm.ps1
@@ -2,5 +2,5 @@ $myshell = New-Object -ComObject wscript.shell;
 $minutes = 120
 for ($i = 0; $i -lt $minutes; $i++) {
   Start-Sleep -Seconds 60
-  $myshell.sendkeys(&quot;.&quot;)
+  $myshell.sendkeys(".")
 }
