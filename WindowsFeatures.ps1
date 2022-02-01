4bd916da0fd6d802ab8ebb60e118786aec59f245
Jeff Jones <spjeff@spjeff.com>
2022-02-01 13:53:16 -0600

    Latest

---
+++ WindowsFeatures.ps1
@@ -0,0 +1,1 @@
+Add-WindowsFeature Web-Server,Windows-Identity-Foundation,NET-Framework-45-ASPNET,Web-Mgmt-Console,Web-Mgmt-Compat,Web-Metabase,Web-Lgcy-Mgmt-Console,Web-Lgcy-Scripting,Web-Mgmt-Tools,Web-WMI,Web-Common-HTTP,NET-HTTP-Activation,NET-Non-HTTP-Activ,NET-WCF-HTTP-Activation45 -Source 'E:\sources\sxs'
