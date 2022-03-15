7da59ae66779a6ad50a7ac198ef5072304f4749f
Jeff Jones <spjeff@spjeff.com>
2022-03-15 14:49:45 -0500

    Update SPRebootFarm.ps1
    .

--- SPRebootFarm.ps1
+++ SPRebootFarm.ps1
@@ -2,5 +2,5 @@ Add-PSSnapIn "Microsoft.SharePoint.PowerShell"
 $local = $env:COMPUTERNAME
 $localFQDN = $env:COMPUTERNAME + "." + $env:USERDNSDOMAIN
 $targets = Get-SPServer |? {$_.Role -ne "Invalid"} |? {$_.Address -ne $local -and $_.Address -ne $localFQDN} | Select Address
-$targets |% {Write-Host "Rebooting $($_)"; Restart-Computer $_ -Force}
+$targets |% {$fqdn = "$($_.Address).$($env:USERDNSDOMAIN)"; Write-Host "Rebooting $fqdn"; Restart-Computer $fqdn -Force}
 Restart-Computer -Force
