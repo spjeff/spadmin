162bced9bd4122ac09685dcdb2bb72634e5534b4
Jeff Jones <spjeff@spjeff.com>
2022-07-26 21:37:38 -0500

    GPEDIT

---
+++ GPEDIT-LDAP-Client-Integrity-Signing.ps1
@@ -0,0 +1,45 @@
+# REGEDIT Quick Read (Default Negotiate)
+Get-ItemPropertyValue -Path 'HKLM:\SYSTEM\CurrentControlSet\Services\LDAP\' -Name 'LDAPClientIntegrity'
+
+# REGEDIT Quick Write (Require Signed)
+Set-ItemProperty -Path 'HKLM:\SYSTEM\CurrentControlSet\Services\LDAP\' -Name 'LDAPClientIntegrity' -Value 2
+
+# from https://stackoverflow.com/questions/23260656/modify-local-security-policy-using-powershell
+Function Parse-SecPol($CfgFile){ 
+    secedit /export /cfg "$CfgFile" | out-null
+    $obj = New-Object psobject
+    $index = 0
+    $contents = Get-Content $CfgFile -raw
+    [regex]::Matches($contents,"(?<=\[)(.*)(?=\])") | %{
+        $title = $_
+        [regex]::Matches($contents,"(?<=\]).*?((?=\[)|(\Z))", [System.Text.RegularExpressions.RegexOptions]::Singleline)[$index] | %{
+            $section = new-object psobject
+            $_.value -split "\r\n" | ?{$_.length -gt 0} | %{
+                $value = [regex]::Match($_,"(?<=\=).*").value
+                $name = [regex]::Match($_,".*(?=\=)").value
+                $section | add-member -MemberType NoteProperty -Name $name.tostring().trim() -Value $value.tostring().trim() -ErrorAction SilentlyContinue | out-null
+            }
+            $obj | Add-Member -MemberType NoteProperty -Name $title -Value $section
+        }
+        $index += 1
+    }
+    return $obj
+}
+
+Function Set-SecPol($Object, $CfgFile){
+   $SecPool.psobject.Properties.GetEnumerator() | %{
+        "[$($_.Name)]"
+        $_.Value | %{
+            $_.psobject.Properties.GetEnumerator() | %{
+                "$($_.Name)=$($_.Value)"
+            }
+        }
+    } | out-file $CfgFile -ErrorAction Stop
+    secedit /configure /db c:\windows\security\local.sdb /cfg "$CfgFile" /areas SECURITYPOLICY
+}
+
+# READ ONLY SAFE
+$SecPool = Parse-SecPol -CfgFile C:\temp\temp.cgf
+
+# DISABLE WRITE
+# Set-SecPol -Object $SecPool -CfgFile C:\temp\temp.cfg
